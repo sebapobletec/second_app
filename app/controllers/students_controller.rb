3b447d111fe80191c74ef4c480a6d3bf9917bae8
Sebastian Poblete <sebapobletec@gmail.com>
2019-01-16 20:57:58 -0300

    Add navbar and edit views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -70,5 +70,5 @@ class StudentsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
       params.require(:student).permit(:name, :age, :email)
-    end
+    end 
 end
---
+++ app/models/studentname.rb
@@ -0,0 +1,2 @@
+class Studentname < ApplicationRecord
+end
